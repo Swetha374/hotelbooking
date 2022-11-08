fe34f12869fa996749d54b1874c1934084126fb0
swetha <swethasasi374@gmail.com>
2022-11-08 18:11:49 +0530

    #2 update model

--- Hotel/models.py
+++ Hotel/models.py
@@ -1,3 +1,63 @@
 from django.db import models
+from django.contrib.auth.models import User, AbstractBaseUser
+from django.core.validators import MaxValueValidator,MinValueValidator
 
-# Create your models here.
+class BaseUser(AbstractBaseUser):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    dob=models.DateField()
+    OPTIONS=(
+        ("male","male"),
+        ("female","female"),
+        ("others","others")
+    )
+    gender=models.CharField(max_length=20,choices=OPTIONS)
+    mobile=models.CharField(max_length=12)
+    email=models.EmailField(verbose_name='email address',max_length=200,unique=True)
+    address=models.TextField(max_length=120)
+    CHOICE=(
+        ("Admin","Admin"),
+        ("Hotel","Hotel"),
+        ("Guest","Guest")
+    )
+    type_of_user=models.CharField(choices=CHOICE)
+
+
+class Hotel(models.Model):
+    name=models.CharField(max_length=120)
+    image=models.ImageField(upload_to="images",null=True)
+    address=models.TextField(max_length=120)
+    location=models.CharField(max_length=50)
+    star_rating=models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    owner_name=models.CharField(max_length=50)
+    owner_image=models.ImageField(upload_to="owner_images",null=True,blank=True)
+    owner_email=models.EmailField(null=True)
+    owner_mobile=models.CharField(max_length=12)
+
+class Room(models.Model):
+    room_name=models.CharField(max_length=20)
+    OPTIONS=(
+        ("Booked","Booked"),
+        ("Open","Open"),
+
+    )
+    availability=models.CharField(choices=OPTIONS,default="Open")
+    price=models.PositiveIntegerField()
+    hotel=models.ForeignKey(Hotel,on_delete=models.CASCADE)
+    occupancy_adult=models.PositiveIntegerField()
+    occupancy_child=models.PositiveIntegerField()
+
+
+class Booking(models.Model):
+    OPTIONS=(
+        ("Pending","Pending"),
+        ("Accepted","Accepted"),
+        ("Rejected","Rejected"),
+        ("Expired","Expired")
+    )
+    status=models.CharField(choices=OPTIONS)
+    occupancy_adult = models.PositiveIntegerField()
+    occupancy_child = models.PositiveIntegerField()
+    guest=models.ForeignKey(BaseUser,on_delete=models.CASCADE)
+    room=models.ForeignKey(Room,on_delete=models.CASCADE)
+    stay_start_date=models.DateField()
+    stay_end_date=models.DateField()
